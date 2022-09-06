050594578e325f61e94634ed0d210dcf1688ab92
dan <dannaor2001@gmail.com>
2022-09-06 16:59:28 +0300

    wip/ created a basic api request

--- back-end/server.js
+++ back-end/server.js
@@ -4,4 +4,6 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(port,() => console.log('hi im listening on port '+ port))
+app.use("/api/example", require("./routes/programRoute"))
+
+app.listen(port,() => console.log(`im listening on port - ${port}`))
