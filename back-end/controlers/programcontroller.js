24a3e716f2d82b981195f00b11613cba59558bf1
dan <dannaor2001@gmail.com>
2022-09-08 20:55:16 +0300

    feat/made connection to cluster, made endpoints async using express-async-handler, created a script to run postman from this dir

--- back-end/controlers/programcontroller.js
+++ back-end/controlers/programcontroller.js
@@ -1,31 +1,32 @@
+const asyncHandler =require("express-async-handler")
 //@desc get a program
 //@route GET /api/program
-const getProgram =(req,res) => {
+const getProgram =asyncHandler(async(req,res) => {
     res.status(200).json({message:'GET program'})
-}
+})
 
 //@desc post a program
 //@route POST /api/program
-const postProgram =(req,res) => 
+const postProgram =asyncHandler(async(req,res) => 
 {
     if(!req.body.text){
         res.status(400)
         throw new Error("add key field please")
     }
     res.status(200).json({message:`POST program`})
-}
+})
 
 //@desc put a program
 //@route PUT /api/program:id
-const putProgram =(req,res) => {
+const putProgram =asyncHandler(async(req,res) => {
     res.status(200).json({message:`PUT program${req.params.id}`})
-}
+})
 
 //@desc delete a program
 //@route DELETE /api/program:id
-const deleteProgram =(req,res) => {
+const deleteProgram =asyncHandler(async(req,res) => {
     res.status(200).json({message:`DELETE program${req.params.id}`})
-}
+})
 
  module.exports={
     getProgram,
--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,11 @@ const port = process.env.PORT || 5000
 const colors = require("colors")
 const app = express()
 const {errorHandler} =require("./middleware/errorMiddleware")
+const connectDB =require("./config/db")
+
+connectDB()
+
+
 //@desc middleware for parsing incoming requests with JSON payloads 
 app.use(express.json())
 
