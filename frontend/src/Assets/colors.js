f90be61a6f0d742218567315bfb8d391f389860f
dan <dannaor2001@gmail.com>
2022-09-20 16:27:11 +0300

    feat/created first pagegit add .! fuck css

---
+++ frontend/src/Assets/colors.js
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+
+const colors = createTheme({
+   palette:{
+    main: {
+        main: '#9F7E37',
+    },
+    secondary:{
+        main:'#000000'
+    },
+    comments:{
+        main:'#7B6F72',
+    },
+    backround:{
+        main:"#FFFFFF"
+    }
+  }
+});
+        
+export default colors
--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -1,12 +1,25 @@
 import  logo from '../Assets/WelcomeIcon.png'
+import Button from '@mui/material/Button';
+import colors from '../Assets/colors';
+import { ThemeProvider } from '@mui/material';
 function WelcomePage() {
   return (
     <div className="WelcomePage">
      <img className='logo' src={logo} ></img>
      <div className='text_section' >
-      <button className='start_btn' ></button>  
+    
+      <text className='welcome_Text'>Welcome</text>
+      <text className='text_desc'>dan’s personal app for logging exercises stats and implementing progressive overload</text>
+      <ThemeProvider theme={colors}>
+      <Button className='start_btn' style={{ fontSize: '40px' ,borderRadius:'100px'}} color='main' variant='contained'>LETS GO!!</Button>
+      </ThemeProvider>
+      <text className='text_quote'>“When you hit failure, your workout has just begun”
+- Ronnie Coleman.</text>
+      {/* <button className='start_btn' >LETS GO!!</button>   */}
+     
      </div>
     </div>
+   
   );
 }
 
